10fa34dce307037a2ea7b53c1ba4f7441405439e
MartinObert <martin.o@vrlife.cz>
2020-03-05 15:57:43 +0100

    Canvas scaling, infocard prefab

--- Runtime/Scripts/InfoCardBody.cs
+++ Runtime/Scripts/InfoCardBody.cs
@@ -1,11 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-namespace  Vrlife.Core
+namespace Vrlife.Core
 {
-   
     public class InfoCardBody : MonoBehaviour
     {
-    
-    }
+        [SerializeField] private Transform container;
 
+        private List<GameObject> _content;
+
+        private void Awake()
+        {
+            _content = new List<GameObject>();
+        }
+
+        public void AddContent(GameObject go)
+        {
+            _content.Add(go);
+            go.transform.SetParent(container);
+        }
+        
+        public void Clear()
+        {
+            _content.Clear();
+            container.ClearChildren();
+        }
+    }
 }
