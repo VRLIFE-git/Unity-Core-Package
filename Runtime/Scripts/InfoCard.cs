e84f60ab6259dd742e090a4fbd292fb0245d52fd
Pozitrone <jirirkas.skola@gmail.com>
2020-03-05 14:24:39 +0100

    First build

---
+++ Runtime/Scripts/InfoCard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zenject;
+
+public class InfoCard : MonoBehaviour
+{
+    public UnityEvent onCloseCard;
+    
+    private IInfoCardManager _infoCardManager;
+
+    [Inject]
+    public void Construct(InfoCardManager.Factory factory)
+    {
+        _infoCardManager = factory.Create(this);
+    }
+    
+    private void Start()
+    {
+        if (onCloseCard == null)
+        {
+            onCloseCard = new UnityEvent();
+        }
+    }
+
+    public void CloseCard()
+    {
+        _infoCardManager.CloseCard();
+        InvokeCardClose();
+    }
+
+
+    // Called by buttons
+    private  void InvokeCardClose()
+    {
+        onCloseCard.Invoke();
+    }
+}
---
+++ Runtime/Scripts/InfoCardManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class InfoCardManager : IInfoCardManager
+{
+    private readonly InfoCard _infoCard;
+
+    public InfoCardManager(InfoCard infoCard)
+    {
+        _infoCard = infoCard;
+    }
+
+    public void CloseCard()
+    {
+        Object.Destroy(_infoCard.gameObject);
+    }
+
+    public class Factory : PlaceholderFactory<InfoCard, InfoCardManager>
+    {
+        public override InfoCardManager Create(InfoCard infoCard)
+        {
+            return new InfoCardManager(infoCard);
+        }
+    }
+}
