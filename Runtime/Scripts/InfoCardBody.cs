9516df5f40d5195107d5ef24227f0a2d80790bec
Pozitrone <jirirkas.skola@gmail.com>
2020-03-06 11:06:01 +0100

    Added option to change color in script

--- Runtime/Scripts/InfoCardBody.cs
+++ Runtime/Scripts/InfoCardBody.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Vrlife.Core
 {
     public class InfoCardBody : MonoBehaviour
     {
         [SerializeField] private Transform container;
+        [SerializeField] private TextMeshProUGUI content;
+        [SerializeField] private Image background;
 
         private List<GameObject> _content;
 
@@ -14,7 +18,7 @@ namespace Vrlife.Core
             _content = new List<GameObject>();
         }
 
-        public void AddContent(GameObject go)
+        public void AddBody(GameObject go)
         {
             _content.Add(go);
             go.transform.SetParent(container);
@@ -25,5 +29,22 @@ namespace Vrlife.Core
             _content.Clear();
             container.ClearChildren();
         }
+        
+        public void SetContent(string value)
+        {
+            content.text = value;
+        }
+
+        public void SetTextColor(Color color)
+        {
+            content.color = color;
+        }
+
+        public void SetBackgroundColor(Color color)
+        {
+            background.color = color;
+        }
+
+        public string GetContent() => content.text;
     }
 }
--- Runtime/Scripts/InfoCardFooter.cs
+++ Runtime/Scripts/InfoCardFooter.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Vrlife.Core
 {
     public class InfoCardFooter : MonoBehaviour
     {
+        [SerializeField] private Image background;
+        
+        public void SetBackgroundColor(Color color)
+        {
+            background.color = color;
+        }
     }
     
 }
