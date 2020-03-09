0eb8bf47748962688dfc7d30eafa6fb206b8880c
Pozitrone <jirirkas.skola@gmail.com>
2020-03-09 10:23:46 +0100

    reworked percentage to coroutine

--- Runtime/Scripts/Loading.cs
+++ Runtime/Scripts/Loading.cs
@@ -1,40 +1,49 @@
-﻿using TMPro;
+﻿using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 [SerializeField]
 public enum LoadingType
-    {
-        Infinite,
-        Percentage
-    }
+{
+    Infinite,
+    Percentage
+}
 public class Loading : MonoBehaviour
 {
     [Header("Required objects")]
     public GameObject loader;
     public TextMeshProUGUI percentText;
     [Tooltip("Can be null, if loading type is set to Infinite")]
-    public AsyncOperation asyncOperation;
+    private AsyncOperation _asyncOperation;
     
     [Header("Settings")]
     public LoadingType loadingType = LoadingType.Infinite;
     [Tooltip("Speed of the loading animation, default 1")]
     public float loadingSpeed = 1f;
-
     
-    private void Start()
-    {
-        if (loadingType == LoadingType.Infinite)
-        {
-            percentText.gameObject.SetActive(false);
-        }
-    }
-
     private void Update()
     {
         loader.transform.eulerAngles = new Vector3(0f, 0f,-(Time.time * loadingSpeed * 100));
-        if (loadingType == LoadingType.Percentage && asyncOperation != null)
-        {
-            percentText.text = (asyncOperation.progress * 100) + " %";
-        }
+        percentText.text = System.Math.Round(_asyncOperation.progress * 100 , 2) + " %";
+    }
+
+    IEnumerator spinner(AsyncOperation asyncOperation)
+    {
+            yield return asyncOperation;
+    }
+
+    public void StartLoading(AsyncOperation asyncOperation)
+    {
+        _asyncOperation = asyncOperation;
+        StartCoroutine(spinner(asyncOperation));
+        loadingType = LoadingType.Percentage;
+    }
+
+    public void StartLoadingInfinite(AsyncOperation asyncOperation)
+    {
+        _asyncOperation = asyncOperation;
+        percentText.gameObject.SetActive(false);
+        StartCoroutine(spinner(asyncOperation));
     }
 }
