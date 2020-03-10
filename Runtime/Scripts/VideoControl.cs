48d05e7bbe43b2e73b2ad47a693387eae0213a01
Pozitrone <jirirkas.skola@gmail.com>
2020-03-10 13:51:26 +0100

    Added videoplayer control

---
+++ Runtime/Scripts/VideoControl.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Vrlife.Core
+{
+    public class VideoControl : MonoBehaviour
+    {
+        private bool paused = false;
+    
+        public void PauseUnpause(VideoPlayer videoPlayer)
+        {
+            if (paused)
+            {
+                videoPlayer.Play();
+                paused = false;
+            }
+            else
+            {
+                videoPlayer.Pause();
+                paused = true;
+            }
+        }
+    }
+}
+
