9c3a413adbc53753ddf9ef5438e1187c1f01d0a4
MartinObert <martin.o@vrlife.cz>
2020-03-11 16:16:55 +0100

    Update PackageWindowUpdater.cs

--- Editor/Scripts/PackageWindowUpdater.cs
+++ Editor/Scripts/PackageWindowUpdater.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using UnityEngine;
 using UnityEditor;
 
 namespace Vrlife.Core.Editor
 {
-    public class PackageWindowUpdater :  EditorWindow
+    [Serializable]
+    public class DependencyJson
+    {
+        public Dictionary<string, string> dependencies;
+    }
+
+
+    public class PackageWindowUpdater : EditorWindow
     {
         string myString = "Hello World";
         bool groupEnabled;
         bool myBool = true;
         float myFloat = 1.23f;
-    
+
         // Add menu item named "My Window" to the Window menu
         [MenuItem("Window/My Window")]
         public static void ShowWindow()
@@ -17,16 +29,29 @@ namespace Vrlife.Core.Editor
             //Show existing window instance. If one doesn't exist, make one.
             EditorWindow.GetWindow(typeof(PackageWindowUpdater));
         }
-    
+
         void OnGUI()
         {
-            GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
-            myString = EditorGUILayout.TextField ("Text Field", myString);
-        
-            groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
-            myBool = EditorGUILayout.Toggle ("Toggle", myBool);
-            myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
-            EditorGUILayout.EndToggleGroup ();
+            var path = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+
+            Debug.Log(File.Exists(path));
+
+            var text = File.ReadAllText(path);
+
+            var obj = JsonConvert.DeserializeObject<DependencyJson>(text);
+            
+            var vrlPackages = obj.dependencies.Where(x => x.Key.StartsWith("com.vrlife"));
+
+            foreach (var keyValuePair in vrlPackages)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(keyValuePair.Key);
+                if (GUILayout.Button("Import"))
+                {
+                    AssetDatabase.ImportPackage(keyValuePair.Value, false);
+                }
+                GUILayout.EndHorizontal();
+            }
         }
     }
 }
