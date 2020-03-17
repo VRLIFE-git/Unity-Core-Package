60358afc813f8c9df4b6186be7e6f750fda94315
MartinObert <martin.o@vrlife.cz>
2020-03-17 16:33:08 +0100

    Update AnimationCommand.cs

--- Runtime/Scripts/Containers/AnimationCommand.cs
+++ Runtime/Scripts/Containers/AnimationCommand.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Vrlife.Core
 {
     [Serializable]
     public class AnimationCommand : Command
     {
+        public AnimationCommand(params string[] watchedStates)
+        {
+            if (watchedStates != null)
+            {
+                WatchedStates = watchedStates.Select(Animator.StringToHash);
+            }
+        }
+        
+        public IEnumerable<int> WatchedStates { get; }
     }
 }
