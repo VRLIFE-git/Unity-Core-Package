e76681434649ece379352776b0cb234410a23f3d
MartinObert <martin.o@vrlife.cz>
2020-03-17 13:49:03 +0100

    Editor and GUI Helpers

---
+++ Editor/Scripts/Editor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Vrlife.Core.Editor
+{
+    public abstract class Editor<TType> : UnityEditor.Editor where TType : class
+    {
+        protected TType Target { get; private set; }
+
+        private void OnEnable()
+        {
+            Target = target as TType;
+        }
+
+        protected IDisposable BeginHorizontal()
+        {
+            return new GUILayout.HorizontalScope();
+        }
+
+        protected IDisposable BeginVertical()
+        {
+            return new GUILayout.VerticalScope();
+        }
+    }
+    
+}
