1459ba61c7b1ebc1dc935688eb0d190f14af1302
MartinObert <martin.o@vrlife.cz>
2020-03-17 17:13:36 +0100

    AudioSourceComponent

---
+++ Runtime/Scripts/Mvc/IAudioSourceComponent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Vrlife.Core.Mvc
+{
+    public interface IAudioSourceComponent : IViewComponent
+    {
+        void PlayOneShot(AudioClip clip);
+        void PlayOneShot(AudioClip clip, float volumeScale);
+    }
+}
---
+++ Runtime/Scripts/Mvc/Implementation/AudioSourceComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Vrlife.Core.Mvc.Abstractions;
+
+namespace Vrlife.Core.Mvc.Implementations
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSourceComponent : AViewComponent, IAudioSourceComponent
+    {
+        [SerializeField] private AudioSource audioSource;
+        protected override void OnAwoke()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayOneShot(AudioClip clip)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+
+        public void PlayOneShot(AudioClip clip, float volume)
+        {
+            audioSource.PlayOneShot(clip, volume);
+        }
+    }
+}
