70379773b0a7959fe0c051074532a170e9f90099
MartinObert <martin.o@vrlife.cz>
2020-03-17 16:17:41 +0100

    Caching

---
+++ Runtime/Scripts/Containers/StateWatcherBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Vrlife.Core.Mvc.Implementations;
+
+namespace Vrlife.Core
+{
+    public class StateWatcherBehavior : StateMachineBehaviour
+    {
+        private Animator _animator;
+        private MonoAnimator _monoAnimator;
+        
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_animator != animator)
+            {
+                _animator = animator;
+                _monoAnimator = animator.GetComponent<MonoAnimator>();
+            }
+            
+            if (_monoAnimator)
+            {
+                _monoAnimator.InvokeStateChanged(stateInfo, layerIndex);
+            }
+            
+            base.OnStateExit(animator, stateInfo, layerIndex);
+        }
+    }
+}
--- Runtime/Scripts/Containers/WalkCommand.cs
+++ Runtime/Scripts/Containers/WalkCommand.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UnityEngine;
-using Vrlife.Core.Mvc.Implementations;
 
 namespace Vrlife.Core
 {
@@ -8,18 +7,5 @@ namespace Vrlife.Core
     public class WalkCommand : Command
     {
         public Transform Target { get; set; }
-    }
-
-    public class StateWatcherBehavior : StateMachineBehaviour
-    {
-        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            var monoanimator = animator.GetComponent<MonoAnimator>();
-            if (monoanimator)
-            {
-                monoanimator.InvokeStateChanged(stateInfo, layerIndex);
-            }
-            base.OnStateExit(animator, stateInfo, layerIndex);
-        }
     }
 }
