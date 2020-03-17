96547cff415d4a9561c493fc2d53182e417b0885
MartinObert <martin.o@vrlife.cz>
2020-03-17 16:52:30 +0100

    CorroutineProcessors

---
+++ Runtime/Scripts/Mvc/ICoroutineProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Vrlife.Core.Mvc
+{
+    public interface ICoroutineProcessor
+    {
+        Coroutine Process(IEnumerator logic, Action onCompleted);
+    }
+}
---
+++ Runtime/Scripts/Mvc/Implementation/CoroutineProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Vrlife.Core.Mvc.Implementations
+{
+    public class CoroutineProcessor : MonoBehaviour, ICoroutineProcessor, IDisposable
+    {
+        private IEnumerator CoroutineProcessing(IEnumerator logic, Action onCompleted)
+        {
+            yield return logic;
+            
+            onCompleted?.Invoke();
+        }
+        
+        public Coroutine Process(IEnumerator logic, Action onCompleted)
+        {
+            var coroutine = StartCoroutine(CoroutineProcessing(logic, onCompleted));
+
+            return coroutine;
+        }
+
+        public void Dispose()
+        {
+            StopAllCoroutines();
+        }
+    }
+}
