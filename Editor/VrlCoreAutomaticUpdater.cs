b72edebd3ff3f920760a457148f6371c3f1f3a25
Pozitrone <jirirkas.skola@gmail.com>
2021-02-25 13:45:59 +0100

    Removed pointless editor scripts, added autoupdater

---
+++ Editor/VrlCoreAutomaticUpdater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace Plugins.com.vrlife.core.Editor
+{
+    public class VrlCoreAutomaticUpdater
+    {
+        [InitializeOnLoad]
+        public class AutomaticUpdate
+        {
+            static AutomaticUpdate()
+            {
+                Client.Add("https://github.com/virtual-real-life/Unity-Core-Package.git");
+            }
+        }
+    }
+}
