a11e26c92e0130742ed0b10b9d21bd4c9a713d18
Pozitrone <jirirkas.skola@gmail.com>
2021-03-09 10:42:17 +0100

    Re-added editor folder

---
+++ Editor/Scripts/MonoKinematicAnimatorComponent.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Vrlife.Core.Mvc;
+
+namespace Vrlife.Core.Editor
+{
+    [CustomEditor(typeof(MonoKinematicAnimatorComponent))]
+    public class MonoKinematicAnimatorComponentEditor : Editor<MonoKinematicAnimatorComponent>
+    {
+        private void OnSceneGUI()
+        {
+            if (!serializedObject.GetBool(MonoKinematicAnimatorComponent.DrawGizmosName))
+            {
+                return;
+            }
+            
+            
+            if (Target.LookAtTarget)
+            {
+                CreateHandleFor(MonoKinematicAnimatorComponent.LookAtName);
+            }
+
+            if (Target.RightHand.positionTarget)
+            {
+                CreateHandleFor(MonoKinematicAnimatorComponent.RightHandTargetName);
+            }
+            
+            if (Target.LeftHand.positionTarget)
+            {
+                CreateHandleFor(MonoKinematicAnimatorComponent.LeftHandTargetName);
+            }
+            
+            if (Target.RightFoot.positionTarget)
+            {
+                CreateHandleFor(MonoKinematicAnimatorComponent.RightFootTargetName);
+            }
+            
+            if (Target.LeftFoot.positionTarget)
+            {
+                CreateHandleFor(MonoKinematicAnimatorComponent.LeftFootTargetName);
+            }
+        }
+
+        private void CreateHandleFor(string propName)
+        {
+            EditorGUI.BeginChangeCheck();
+            var t = serializedObject.Get<Transform>(propName);
+            
+            var lookAt = Handles.PositionHandle(t.position, t.rotation);
+            
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(Target, "Change Look At Target Position");
+
+
+                t.position = lookAt;
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+
+    public static class SerializedObjectHelpers
+    {
+        public static TValue Get<TValue>(this SerializedObject serializedObject, string name) where TValue : Object
+        {
+            return serializedObject.FindProperty(name).objectReferenceValue as TValue;
+        } public static bool GetBool(this SerializedObject serializedObject, string name)
+        {
+            return serializedObject.FindProperty(name).boolValue;
+        }
+    }
+}
---
+++ Editor/Scripts/PackageWindowUpdater.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEditor;
+
+namespace Vrlife.Core.Editor
+{
+    public class GuiHorizontalHelpers : IDisposable
+    {
+        public GuiHorizontalHelpers()
+        {
+            EditorGUILayout.BeginHorizontal();
+        }
+        public void Dispose()
+        {
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    public static class EditorWindowHelpers
+    {
+        public static GuiHorizontalHelpers Horizontal(this EditorWindow window)
+        {
+            return new GuiHorizontalHelpers();
+        }
+
+        public static bool Button(this EditorWindow window, string name, params GUILayoutOption[] options)
+        {
+            return GUILayout.Button(name, options);
+        }
+        
+        public static void Label(this EditorWindow window, string name, params GUILayoutOption[] options)
+        {
+             GUILayout.Label(name, options);
+        }
+    }
+    
+    public class PackageWindowUpdater : EditorWindow
+    {
+       
+        // Add menu item named "My Window" to the Window menu
+        [MenuItem("Window/VRLIFE - Package updater")]
+        public static void ShowWindow()
+        {
+            //Show existing window instance. If one doesn't exist, make one.
+            GetWindow(typeof(PackageWindowUpdater), true, "VRLIFE - Package Updater", true);
+        }
+
+        private void OnEnable()
+        {
+            this.minSize = new Vector2(300, 300);
+            this.maxSize = new Vector2(300, 300);
+        }
+
+        void OnGUI()
+        {
+            
+            
+            var path = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+            GUILayout.Label("Packages in ../Packages/manifest.json");
+            EditorGUILayout.Separator();
+
+            if (!File.Exists(path))
+            {
+                EditorGUILayout.HelpBox("Manifest " + "../Packages/manifest.json was not found", MessageType.Warning);
+                return;
+            }
+            var text = File.ReadAllText(path);
+            
+            var obj = JsonConvert.DeserializeObject<DependencyJson>(text);
+
+            if (obj.@lock == null)
+            {
+                EditorGUILayout.HelpBox("No locked packages from github are in project.", MessageType.Warning);
+
+                return;
+            }
+            
+            foreach (var keyValuePair in obj.@lock)
+            {
+                using (this.Horizontal())
+                {
+                    this.Label(keyValuePair.Key);
+                    
+                    if (this.Button("Import", GUILayout.Width(50)))
+                    {
+                        obj.@lock.Remove(keyValuePair.Key);
+                        var manifest = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                    
+                        File.WriteAllText(path, manifest);
+                        AssetDatabase.Refresh();
+                        break;
+                    }
+                }
+                
+            }
+        }
+    }
+}
